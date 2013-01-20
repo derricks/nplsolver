39a40d7dc31e9ba569aafb6053f3fcdc5510cc0b
Derrick Schneider <derrick.schneider@gmail.com>
2013-01-20 12:04:52 -0800

    Make the server work with some simplifications. More complex bits can come now that functionality is in place

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "net"
   "time"
+  "io"
   "encoding/binary"
   "encoding/json"
   "nplsolver/properties"
@@ -14,20 +15,21 @@ import (
 
 const (
    connectionTimeoutProp = "server.connection.timeout.seconds"
+   maxMessageSize = 512
 )
 
 // the query object we might expect from JSON on the socket
 type query struct{
-   puzzleType string // e.g., transposal or letterbank
-   puzzleText string // the text to search for 
-   dictionary string // which dictionary to use
-   options map[string]interface{} // any solver specific additional options (e.g., which letter change in a letter change) or extra information needed by the query
+   PuzzleType string // e.g., transposal or letterbank
+   PuzzleText string // the text to search for 
+   Dictionary string // which dictionary to use
+   Options map[string]interface{} // any solver specific additional options (e.g., which letter change in a letter change) or extra information needed by the query
 }
 
 type response struct {
-   request query
-   err     error
-   results []string  
+   Request query
+   Err     error
+   Results []string  
 }
 
 // this method will never return unless there's an error!
@@ -40,7 +42,7 @@ func StartServer() error {
   for { // loop forever!
     conn, err := listener.Accept()
     if err != nil {
-       return err
+       continue
     }
     
     go handleConnection(conn)  
@@ -50,8 +52,7 @@ func StartServer() error {
 
 func handleConnection(conn net.Conn) {
 
-    //close the connection, no matter what
-    defer conn.Close()
+   defer conn.Close()
 
    // make sure the connection won't hang with a timeout
    timeout, err := properties.GetInteger(connectionTimeoutProp)
@@ -62,38 +63,24 @@ func handleConnection(conn net.Conn) {
    // set deadline to now plus timeout
    conn.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
    
-   //parse the message: it assumes that the first 2 bytes gives you a size of the incoming string
-   messageLength,err := getMessageLength(conn)
-   if err != nil || messageLength == 0 {
-      return
-   }
-   
-   //  the string is assumed to be JSON
-   buf := make([]byte,0,messageLength)
-   bytesRead, err := conn.Read(buf)
-   
-   if err != nil || bytesRead == 0 {
-      return
-   }
-   
-   jsonData := new(query)
-   err = json.Unmarshal(buf,&jsonData)
+   jsonData,err := readData(conn)
    if err != nil {
+     logError("Did not unmarshal json data",err)
       return
    }
-   
       
-   solver,err := solver.GetSolver(solver.SolverType(jsonData.puzzleType))   
+   solver,err := solver.GetSolver(solver.SolverType(jsonData.PuzzleType))   
    if err != nil {
-      err = writeResponse(conn, response{*jsonData,err,nil})
+      logError("Couldn't get solver",err)
+      err = writeResponse(conn, response{jsonData,err,nil})
       if err != nil {
          return
       }
    }
    
-   dict,err := dict.FindDictionaryByName(jsonData.dictionary)
+   dict,err := dict.FindDictionaryByName(jsonData.Dictionary)
    if err != nil {
-      err = writeResponse(conn,response{*jsonData,err,nil})
+      err = writeResponse(conn,response{jsonData,err,nil})
       if err == nil {
          return
       }
@@ -103,7 +90,7 @@ func handleConnection(conn net.Conn) {
    results := make([]string,0,100)
    resultsChan := make(chan string)
    doneChan := make(chan interface{})
-   go solver.Solve(jsonData.puzzleText,dict,resultsChan,doneChan)
+   go solver.Solve(jsonData.PuzzleText,dict,resultsChan,doneChan)
    
    done := false
    for !done {
@@ -114,24 +101,51 @@ func handleConnection(conn net.Conn) {
                done = true
                switch finishItem.(type) {
                   case error:
-                     writeResponse(conn,response{*jsonData,err,nil}) // no err trapping currently needed here; the connection's about to close anyway
+                     writeResponse(conn,response{jsonData,err,nil}) // no err trapping currently needed here; the connection's about to close anyway
                   default: // success at last!
-                    writeResponse(conn,response{*jsonData,nil,results})   
+                    err = writeResponse(conn,response{jsonData,nil,results})   
+                    if err != nil {
+                        logError("Couldn't write response to client",err)
+                    }
                }
        }
    }
 }
 
+func logError(message string, err error) {
+   fmt.Printf("Error: %v %v\n",message,err)
+}
+
+// pulls the json data off of a connection
+func readData(conn net.Conn) (query,error) {
+    
+   buf := make([]byte,maxMessageSize)
+   
+   // read all the data off the connection
+   bytesRead, err := conn.Read(buf)
+   
+   if (err != nil && err != io.EOF) || bytesRead == 0 {
+      return *new(query),err
+   }
+   
+   // unmarshal the relevant bytes into json structure
+   jsonData := new(query)
+   err = json.Unmarshal(buf[0:bytesRead],&jsonData)
+   if err != nil {
+      return *new(query),err
+   }
+   
+   return *jsonData,nil
+}
+
 // takes a response object and writes it back down the socket with the same format (2 bytes for length and then json string)
 func writeResponse(conn net.Conn, res response) error {
+  fmt.Printf("Sending back %v\n",res)
   bytes,err := json.Marshal(res)
   if err != nil {
      return err
   }
-  err = binary.Write(conn,binary.BigEndian,int16(len(bytes)))
-  if err != nil {
-     return err
-  }
+  
   _,err = conn.Write(bytes)
   if err != nil {
      return err
@@ -141,8 +155,8 @@ func writeResponse(conn net.Conn, res response) error {
 }
 
 // given a connection, reads the first two bytes and converts them to a 16-bit integer
-func getMessageLength(conn net.Conn) (int16,error) {
-   var retval int16
+func getMessageLength(conn net.Conn) (uint16,error) {   
+   var retval uint16
    err := binary.Read(conn,binary.BigEndian,&retval)
    return retval,err
 }
